60f7c7c26f170ab6329d457571ebe8b52b863071
David Majidian <hosseinm@nutranext.net>
2019-06-08 16:11:10 -0700

    no message

--- src/PosScan/TerminalException.php
+++ src/PosScan/TerminalException.php
@@ -3,6 +3,10 @@ namespace PosScan;
 
 class TerminalException extends \Exception
 {
+    /**
+     * TerminalException constructor.
+     * @param $message
+     */
     public function __construct($message)
     {
         parent::__construct($message);
